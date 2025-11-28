a1e90b53efd731edf617c9dd12a922058938132b
osigosi <waltercandy27@gmail.com>
2025-11-28 16:14:33 +0100

    Insert teaching activities and person records

---
+++ insert.sql
@@ -0,0 +1,39 @@
+
+--Insert
+INSERT INTO teaching_activity (activity_name, factor)
+VALUES
+('LECTURE', 3.6),
+('LAB', 2.4),
+('TUTORIAL', 2.4),
+('SEMINAR', 1.8);
+
+
+INSERT INTO person (last_name,phone_no,adress_name,zip,city,country,personal_number,first_name)
+VALUES
+  ('Savage','0753019280','8669 Nascetur Rd.','47642','Vänersborg','Sweden','191403088562','Annika'),
+  ('Graves','0746562393','7633 Fusce Rd.','87844','Borlänge','Sweden','200406016078','Steffi'),
+  ('Bridges','0778497231','Ap #979-9295 Eget Road','77605','Stockholm','Sweden','19171228597','Ricarda'),
+  ('Gonzales','0712730445','Ap #175-7718 Porta Rd.','59858','Alingsås','Sweden','200905078751','Janette'),
+  ('Williams','0762160476','8817 Sagittis Av.','83762','Bollnäs','Sweden','200201083443','Florian'),
+  ('Mcgee','0744078377','Ap #685-1799 In Street','73841','Finspång','Sweden','200907064833','Annika'),
+  ('Hays','0715682218','Ap #995-402 Et, Av.','23318','Mjölby','Sweden','200407029826','Vanessa'),
+  ('Hendricks','0753435155','Ap #177-1989 Condimentum St.','00461','Upplands Väsby','Sweden','19981225356','Lina'),
+  ('James','0724543873','729-9016 Neque Avenue','48048','Alingsås','Sweden','19131225225','Erna'),
+  ('Spence','0781578685','Ap #234-9600 Lacinia St.','68364','Alingsås','Sweden','200701056848','Jacqueline'),
+  ('Workman','0706687786','Ap #811-7516 Ornare, Ave','43815','Borås','Sweden','191407075387','Sylvia'),
+  ('Hoffman','0716821464','8974 Iaculis Ave','59320','Vetlanda','Sweden','19441225356','Florian'),
+  ('Dillard','0751516823','8252 Lectus Road','62567','Mora','Sweden','198606083485','Karen'),
+  ('Wolfe','0741404263','8145 Mauris St.','44571','Tranås','Sweden','19851229424','Frieda'),
+  ('Bryan','0744153680','P.O. Box 666, 874 Aliquam Av.','47057','Upplands Väsby','Sweden','19621223877','Benedikt'),
+  ('Hodges','0792004636','335-2776 Sit Road','88434','Söderhamn','Sweden','200906061773','Diana'),
+  ('Fox','0718638812','Ap #638-9786 Eros St.','46943','Nässjö','Sweden','194606005527','Horst'),
+  ('Mejia','0784065322','Ap #176-3224 Nullam Av.','22208','Vetlanda','Sweden','200904078530','Rainer'),
+  ('Wilkerson','0781301232','120-1422 Imperdiet Rd.','82637','Åkersberga','Sweden','198501089231','Uta'),
+  ('Rose','0744396824','Ap #979-8491 Fusce Road','55421','Mora','Sweden','195504061453','Nils'),
+  ('Lane','0748226866','Ap #101-6151 Vivamus St.','48628','Mjölby','Sweden','200108042043','Dietrich'),
+  ('Alvarez','0756559818','P.O. Box 468, 6789 Quisque Road','98541','Norrköping','Sweden','200802080642','Wilhelm'),
+  ('Hebert','0767114574','118-3664 Faucibus Av.','67861','Värnamo','Sweden','19681222781','Boris'),
+  ('Jordan','0753296953','Ap #426-7126 Egestas. Road','47364','Mora','Sweden','19711225188','Susanne'),
+  ('Foster','0723253452','167-7904 Nulla. Avenue','74684','Ludvika','Sweden','19461223453','Carolin');
+
+
