b9105d5ca6e4c355133c1baa657995aca2c0c177
osigosi <waltercandy27@gmail.com>
2025-11-30 22:10:36 +0100

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,4 +1,11 @@
 
+INSERT INTO department (department_name)
+VALUES
+('Computer Science'),
+('Mathematics'),
+('Electrical Engineering'),
+('Human Centered Technology'),
+('Intelligent Systems');
 --Insert
 INSERT INTO teaching_activity (activity_name, factor)
 VALUES
@@ -7,18 +14,13 @@ VALUES
 ('TUTORIAL', 2.4),
 ('SEMINAR', 1.8);
 
-
-INSERT INTO person (last_name,phone_no,adress_name,zip,city,country,personal_number,first_name)
+INSERT INTO person (last_name, phone_no, adress_name, zip, city, country, personal_number, first_name)
 VALUES
-  ('Savage','0753019280','8669 Nascetur Rd.','47642','Vänersborg','Sweden','191403088562','Annika'),
-  ('Graves','0746562393','7633 Fusce Rd.','87844','Borlänge','Sweden','200406016078','Steffi'),
-  ('Bridges','0778497231','Ap #979-9295 Eget Road','77605','Stockholm','Sweden','19171228597','Ricarda'),
-  ('Gonzales','0712730445','Ap #175-7718 Porta Rd.','59858','Alingsås','Sweden','200905078751','Janette'),
-  ('Williams','0762160476','8817 Sagittis Av.','83762','Bollnäs','Sweden','200201083443','Florian'),
-  ('Mcgee','0744078377','Ap #685-1799 In Street','73841','Finspång','Sweden','200907064833','Annika'),
-  ('Hays','0715682218','Ap #995-402 Et, Av.','23318','Mjölby','Sweden','200407029826','Vanessa'),
-  ('Hendricks','0753435155','Ap #177-1989 Condimentum St.','00461','Upplands Väsby','Sweden','19981225356','Lina'),
-  ('James','0724543873','729-9016 Neque Avenue','48048','Alingsås','Sweden','19131225225','Erna'),
+  ('Linbäck','0787904425','Kthgatan 23','11855','Stockholm','Sweden','195201010000','Leif'),
+  ('Carbone','0731234567','Kthgatan 1','11655','Stockholm','Sweden','198201010000','Paris'),
+  ('Gauraha','0731236567','Kthgatan 2','11255','Stockholm','Sweden','198201010400','Niharika'),
+  ('Doe','0735236567','Kthgatan 3','11255','Stockholm','Sweden','199201010400','Adam'),
+  ('Doe','0735246567','Kthgatan 4','11255','Stockholm','Sweden','199201010450','Brian'),
   ('Spence','0781578685','Ap #234-9600 Lacinia St.','68364','Alingsås','Sweden','200701056848','Jacqueline'),
   ('Workman','0706687786','Ap #811-7516 Ornare, Ave','43815','Borås','Sweden','191407075387','Sylvia'),
   ('Hoffman','0716821464','8974 Iaculis Ave','59320','Vetlanda','Sweden','19441225356','Florian'),
@@ -35,5 +37,69 @@ VALUES
   ('Hebert','0767114574','118-3664 Faucibus Av.','67861','Värnamo','Sweden','19681222781','Boris'),
   ('Jordan','0753296953','Ap #426-7126 Egestas. Road','47364','Mora','Sweden','19711225188','Susanne'),
   ('Foster','0723253452','167-7904 Nulla. Avenue','74684','Ludvika','Sweden','19461223453','Carolin');
+INSERT INTO course (course_code)
+VALUES
+("CS1010"),
+("CS2020"),
+("IX1500"),
+("IV1351"),
+("ID2214"),
+("IV1350"),
+('DA2000'),
+('DA3000'),
+('DD1320'),
+('ID1018'),
+('IV1360'),
+('SF1624'),
+('SF1688'),
+('IK1550'),
+('IS1200');
+INSERT INTO course_layout (course_name,course_id,hp,version_number,min_students,max_students)
+VALUES
+  ('Computer Science 2',2,7.50,1,20,150),
+  ('Computer Science 2 ',2,7.50,2,25,200),
+  ('Programming for Data Science',6,7.50,1,20,150),
+  ('Programming for Data Science',6,7.50,2,30,200),
+  ('Discrete Mathematics',4,7.50,1,50,200),  
+  ('Discrete Mathematics',4,7.50,2,50,150),
+  ('Data Storage Paradigms',5,7.50,1,50,300),
+  ('Data Storage Paradigms',5,7.50,2,50,250),
+  ('Object Oriented Design',7,7.50,1,20,250),
+  ('Computer Science 1 ',1,7.50,1,25,200),
+  ('Computer Science 1',1,7.50,2,30,150),
+  ('Course DA2000',  (SELECT course_id FROM course WHERE course_code = 'DA2000'), 7.5, 1, 20, 200),
+  ('Course DA3000',  (SELECT course_id FROM course WHERE course_code = 'DA3000'), 7.5, 1, 20, 200),
+  ('Course DD1320',  (SELECT course_id FROM course WHERE course_code = 'DD1320'), 7.5, 1, 20, 200),
+  ('Course ID1018',  (SELECT course_id FROM course WHERE course_code = 'ID1018'), 7.5, 1, 20, 200),
+  ('Course IV1350',  (SELECT course_id FROM course WHERE course_code = 'IV1350'), 7.5, 1, 20, 200),
+  ('Course IV1360',  (SELECT course_id FROM course WHERE course_code = 'IV1360'), 7.5, 1, 20, 200),
+  ('Course SF1624',  (SELECT course_id FROM course WHERE course_code = 'SF1624'), 7.5, 1, 20, 200),
+  ('Course SF1688',  (SELECT course_id FROM course WHERE course_code = 'SF1688'), 7.5, 1, 20, 200),
+  ('Course IK1550',  (SELECT course_id FROM course WHERE course_code = 'IK1550'), 7.5, 1, 20, 200),
+  ('Course IS1200',  (SELECT course_id FROM course WHERE course_code = 'IS1200'), 7.5, 1, 20, 200);
+INSERT INTO employee (employment_id, email, job_id, department_id, person_id)
+VALUES
+('EMID500001', 'leilin@uni.com', 2, 1, 1),   
+('EMID500002', 'parcar@uni.com', 4, 1, 2),   
+('EMID500003', 'nihgau@uni.com', 2, 1, 3),   
+('EMID500004', 'adadoe@uni.com', 3, 1, 4),   
+('EMID500005', 'bridoe@uni.com', 5, 1, 5),   
 
+('EMID500006', 'jacspe@uni.com', 1, 2, 6),
+('EMID500007', 'sylwor@uni.com', 3, 3, 7),
+('EMID500008', 'flohff@uni.com', 5, 4, 8),
+('EMID500009', 'kardil@uni.com', 2, 5, 9),
+('EMID500010', 'friwol@uni.com', 1, 2, 10),
 
+('EMID500011', 'benbry@uni.com', 3, 3, 11),
+('EMID500012', 'dianho@uni.com', 4, 4, 12),
+('EMID500013', 'horsfo@uni.com', 5, 5, 13),
+('EMID500014', 'raimej@uni.com', 2, 2, 14),
+('EMID500015', 'utawil@uni.com', 3, 3, 15),
+
+('EMID500016', 'nilros@uni.com', 1, 4, 16),
+('EMID500017', 'dielan@uni.com', 5, 5, 17),
+('EMID500018', 'wilalv@uni.com', 2, 2, 18),
+('EMID500019', 'borheb@uni.com', 4, 3, 19),
+('EMID500020', 'susjor@uni.com', 3, 4, 20),
+('EMID500021', 'carfos@uni.com', 1, 5, 21);
